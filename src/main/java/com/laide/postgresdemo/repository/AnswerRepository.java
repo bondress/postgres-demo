a6db26cd1d1ea4b6d24e883878fa4958c20d743a
Laide <endeleylaide@gmail.com>
2021-04-20 12:13:37 +0100

    Spring Boot, PostgreSQL, JPA, Hibernate RESTful CRUD API Example

---
+++ src/main/java/com/laide/postgresdemo/repository/AnswerRepository.java
@@ -0,0 +1,10 @@
+package com.laide.postgresdemo.repository;
+
+import com.laide.postgresdemo.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionId(Long questionId);
+}
